a0ea9786afa2e97e968ab06ae3e519f4f00642e1
kxfo <147588035+kxfo12@users.noreply.github.com>
2024-10-09 15:51:36 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,13 +2,35 @@ package com.example.flaga;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
+    private int counter = 0;
+    private int wrongcounter = 0;
+    private TextView textViewtt;
+    @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewtt = findViewById(R.id.textViewtt);
+    }
+
+    public void wrong(View view){
+        Toast.makeText(this, R.string.wrong, Toast.LENGTH_SHORT).show();
+        wrongcounter++;
+    }
+
+    public void correct(View view){
+        Toast.makeText(this, R.string.correct, Toast.LENGTH_SHORT).show();
+        view.setVisibility(View.INVISIBLE);
+        counter++;
+        if(counter == 4){
+            textViewtt.setText(R.string.win + wrongcounter);
+        }
     }
 }
